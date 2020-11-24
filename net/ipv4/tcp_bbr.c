c64ce2286821bd6356848027901f72f4b2e820c3
Imran Khan <imranbuet63@gmail.com>
2020-11-23 20:00:29 -0500

    tcp_offload

--- net/ipv4/tcp_bbr.c
+++ net/ipv4/tcp_bbr.c
@@ -1111,7 +1111,7 @@ static struct tcp_congestion_ops tcp_bbr_cong_ops __read_mostly = {
 	.undo_cwnd	= bbr_undo_cwnd,
 	.cwnd_event	= bbr_cwnd_event,
 	.ssthresh	= bbr_ssthresh,
-	.min_tso_segs	= bbr_min_tso_segs,
+	.tso_segs_goal	= bbr_tso_segs_goal,
 	.get_info	= bbr_get_info,
 	.set_state	= bbr_set_state,
 };
